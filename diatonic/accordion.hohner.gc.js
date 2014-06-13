5f61d0a6bbd24e6b7a952322b96c65749304464a
Flávio Vani <flavio.vani@gmail.com>
2014-06-13 00:25:47 -0300

    mercedita

--- diatonic/accordion.hohner.gc.js
+++ diatonic/accordion.hohner.gc.js
@@ -112,6 +112,7 @@ DIATONIC.tmp.scales = [
 
 DIATONIC.tmp.songs = [
     'songs/jai.abcx'
+   ,'songs/mercedita.abcx'
    ,'songs/primeira.valsinha.abcx'
    ,'songs/valsa.abcx'
    ,'songs/didatica.abcx'
