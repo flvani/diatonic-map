6484d971b513a3cc3cbabdaac377133f95d3e965
Flavio Vani <flavio.vani@gmail.com>
2017-04-11 20:24:20 -0300

    Implementado Versão 4.22: Seleção do texto ABCX durante playback

--- minify.bat
+++ minify.bat
@@ -6,10 +6,12 @@
 @IF [%1]==[] goto :fim
 
 copy /b/y javascripts\language.js+draggable\draggable.js+site\mapa.js+site\estudio.js tmp\site.js
+copy /b/y css\draggable.css+css\mapa.css+css\studio.css tmp\site.css
 
 @set versao=%1
 
 @echo Compressing Site %versao% ...
 java -Dfile.encoding=utf-8 -jar yuicompressor-2.4.2.jar  --line-break 7000 -o site_%versao%-min.js tmp\site.js
+java -Dfile.encoding=utf-8 -jar yuicompressor-2.4.2.jar  --line-break 7000 -o site_%versao%-min.css tmp\site.css
 
 :fim
