dca5adcc45d98f901167fdf78f32ffd4988324fd
Flavio Vani <flavio.vani@gmail.com>
2018-02-04 23:36:42 -0200

    Tio Mederico

--- diatonic/diatonic.js
+++ diatonic/diatonic.js
@@ -10,6 +10,59 @@ if (!window.DIATONIC)
 if (!window.DIATONIC.map)
     window.DIATONIC.map = {};
 
+DIATONIC.map.color = {};
+DIATONIC.map.color.fill = 'none';
+DIATONIC.map.color.background = 'none';
+DIATONIC.map.color.open = '#00ff00';
+DIATONIC.map.color.close = '#00b2ee';
+
+DIATONIC.map.loadAccordionMaps = function ( files, cb )  {
+    
+    if( ! DIATONIC.map.accordionMaps )
+        DIATONIC.map.accordionMaps = [];
+    
+    var toLoad = 0;
+    for( var f = 0; f <  files.length; f ++ ) {
+        toLoad ++;
+        FILEMANAGER.register('MAP');
+
+        $.getJSON( files[f], {  format: "json"  })
+            .done(function( data ) {
+                FILEMANAGER.deregister('MAP', true);
+                DIATONIC.map.accordionMaps.push( new DIATONIC.map.AccordionMap(data) );
+            })
+            .fail(function( data, textStatus, error ) {
+                FILEMANAGER.deregister('MAP', false);
+                var err = textStatus + ", " + error;
+                waterbug.log( "Accordion Load Failed:\nLoading: " + data.responseText.substr(1,40) + '...\nError:\n ' + err );
+            })
+            .always(function() {
+                toLoad --; 
+                if( toLoad === 0 ) {
+                    DIATONIC.map.sortAccordions();
+                    cb && cb();
+                }
+            });
+    }
+};
+
+DIATONIC.map.sortAccordions = function () {
+    DIATONIC.map.accordionMaps.sort( function(a,b) { 
+        return parseInt(a.menuOrder) - parseInt(b.menuOrder);
+    });
+};
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+if (!window.DIATONIC)
+    window.DIATONIC = {};
+
+if (!window.DIATONIC.map)
+    window.DIATONIC.map = {};
+
 DIATONIC.map.AccordionMap = function (res, local) {
     this.id = res.id;
     this.menuOrder = res.menuOrder;
@@ -147,193 +200,6 @@ DIATONIC.map.AccordionMap.prototype.loadABCX = function(pathList, cb ) {
             });
     }
     return objRet;
-};
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-if (!window.DIATONIC)
-    window.DIATONIC = {};
-
-if (!window.DIATONIC.map)
-    window.DIATONIC.map = {};
-
-DIATONIC.map.Button = function( kb, x, y, options ) {
-
-    var opt = options || {};
-    
-    this.kb = kb;
-    this.x = x;
-    this.y = y;
-    
-    this.openNote = null;
-    this.closeNote = null;
-    this.tabButton = null;
-    
-    this.SVG  = {gid: 0}; // futuro identificador
-    
-    this.radius = opt.radius;
-    this.isPedal  = opt.isPedal || false;
-    this.borderWidth = opt.borderWidth || (this.isPedal?2:1);
-    this.borderColor = opt.borderColor || (this.isPedal?'red':'black');
-
-};
-
-DIATONIC.map.Button.prototype.draw = function( id, printer, limits, options ) {
-    
-    var currX, currY;
-
-    if( options.transpose ) {
-        //horizontal
-        currX = this.y;
-        currY = options.mirror ? this.x : limits.maxX - this.radius*2 - (this.x - limits.minX);
-    } else {
-        //vertical
-        currX = options.mirror ? limits.maxX - this.radius*2 - (this.x - limits.minX): this.x;
-        currY = this.y;
-    }
-    
-    options = options || {};
-    options.radius = this.radius;
-    options.borderColor = this.borderColor;
-    options.borderWidth = this.borderWidth;
-    options.fillColor = (options.kls && options.kls === 'blegenda'? 'none' : DIATONIC.map.color.fill );
-    options.openColor = (options.kls && options.kls === 'blegenda'? DIATONIC.map.color.open : 'none' );
-    options.closeColor = (options.kls && options.kls === 'blegenda'? DIATONIC.map.color.close : 'none' );
-    
-    this.SVG.gid = printer.printButton( id, currX, currY, options );
-
-};
-
-DIATONIC.map.Button.prototype.clear = function(delay) {
-    if(!this.SVG.button ) return;
-    var that = this;
-    if(delay) {
-        window.setTimeout(function(){ that.clear(); }, delay*1000);
-        return;
-    }    
-    this.SVG.closeArc.style.setProperty( 'fill', 'none' );
-    this.SVG.openArc.style.setProperty( 'fill', 'none' );
-};
-
-DIATONIC.map.Button.prototype.setOpen = function(delay) {
-    if(!this.SVG.button ) return;
-    var that = this;
-    if(  delay ) {
-        window.setTimeout(function(){that.setOpen();}, delay*1000 );
-        return;
-    } 
-    this.SVG.openArc.style.setProperty( 'fill', DIATONIC.map.color.open );
-};
-
-DIATONIC.map.Button.prototype.setClose = function(delay) {
-    if(!this.SVG.button ) return;
-    var that = this;
-    if(  delay ) {
-        window.setTimeout(function(){that.setClose();}, delay*1000);
-        return;
-    } 
-    this.SVG.closeArc.style.setProperty( 'fill', DIATONIC.map.color.close );
-};
-
-DIATONIC.map.Button.prototype.setSVG = function(showLabel, pull, push, translator ) {
-    var b = this.SVG;
-    this.SVG.button = document.getElementById(b.gid);
-    this.SVG.openArc = document.getElementById(b.gid+'_ao');
-    this.SVG.openText = document.getElementById(b.gid+'_to');
-    this.SVG.closeArc = document.getElementById(b.gid+'_ac');
-    this.SVG.closeText = document.getElementById(b.gid+'_tc');
-    
-    if( translator ) {
-        this.SVG.openText.setAttribute( 'data-translate', pull );
-        this.SVG.closeText.setAttribute( 'data-translate', push );
-        this.setText(showLabel, translator.getResource(pull), translator.getResource(push) ); 
-} else {
-        this.setText(showLabel, pull, push ); 
-    }
-    
-};
-
-DIATONIC.map.Button.prototype.setText = function( showLabel, open, close ) {
-    if(this.SVG.openText) {
-        this.SVG.openText.textContent = open ? open : this.getLabel( this.openNote, showLabel );
-        this.SVG.closeText.textContent = close ? close : this.getLabel( this.closeNote, showLabel );
-    }    
-};
-
-DIATONIC.map.Button.prototype.getLabel = function(nota, showLabel) {
-    var l = nota.key;
-    
-    if (showLabel) {
-        l = l.toUpperCase() + '';
-        l = ABCXJS.parse.key2br[l].toUpperCase();
-    }
-    
-    if ( nota.isChord ) {
-       l = l.toLowerCase() + '';
-    }    
-    
-    if( nota.isMinor ) {
-        l+='-';
-    }
-    return l;
-};
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-if (!window.DIATONIC)
-    window.DIATONIC = {};
-
-if (!window.DIATONIC.map)
-    window.DIATONIC.map = {};
-
-DIATONIC.map.color = {};
-DIATONIC.map.color.fill = 'none';
-DIATONIC.map.color.background = 'none';
-DIATONIC.map.color.open = '#00ff00';
-DIATONIC.map.color.close = '#00b2ee';
-
-DIATONIC.map.loadAccordionMaps = function ( files, cb )  {
-    
-    if( ! DIATONIC.map.accordionMaps )
-        DIATONIC.map.accordionMaps = [];
-    
-    var toLoad = 0;
-    for( var f = 0; f <  files.length; f ++ ) {
-        toLoad ++;
-        FILEMANAGER.register('MAP');
-
-        $.getJSON( files[f], {  format: "json"  })
-            .done(function( data ) {
-                FILEMANAGER.deregister('MAP', true);
-                DIATONIC.map.accordionMaps.push( new DIATONIC.map.AccordionMap(data) );
-            })
-            .fail(function( data, textStatus, error ) {
-                FILEMANAGER.deregister('MAP', false);
-                var err = textStatus + ", " + error;
-                waterbug.log( "Accordion Load Failed:\nLoading: " + data.responseText.substr(1,40) + '...\nError:\n ' + err );
-            })
-            .always(function() {
-                toLoad --; 
-                if( toLoad === 0 ) {
-                    DIATONIC.map.sortAccordions();
-                    cb && cb();
-                }
-            });
-    }
-};
-
-DIATONIC.map.sortAccordions = function () {
-    DIATONIC.map.accordionMaps.sort( function(a,b) { 
-        return parseInt(a.menuOrder) - parseInt(b.menuOrder);
-    });
 };
 /* 
  * To change this license header, choose License Headers in Project Properties.
@@ -602,3 +468,137 @@ DIATONIC.map.Keyboard.prototype.clear = function (full) {
     }
     this.modifiedItems = new Array();
 };
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+if (!window.DIATONIC)
+    window.DIATONIC = {};
+
+if (!window.DIATONIC.map)
+    window.DIATONIC.map = {};
+
+DIATONIC.map.Button = function( kb, x, y, options ) {
+
+    var opt = options || {};
+    
+    this.kb = kb;
+    this.x = x;
+    this.y = y;
+    
+    this.openNote = null;
+    this.closeNote = null;
+    this.tabButton = null;
+    
+    this.SVG  = {gid: 0}; // futuro identificador
+    
+    this.radius = opt.radius;
+    this.isPedal  = opt.isPedal || false;
+    this.borderWidth = opt.borderWidth || (this.isPedal?2:1);
+    this.borderColor = opt.borderColor || (this.isPedal?'red':'black');
+
+};
+
+DIATONIC.map.Button.prototype.draw = function( id, printer, limits, options ) {
+    
+    var currX, currY;
+
+    if( options.transpose ) {
+        //horizontal
+        currX = this.y;
+        currY = options.mirror ? this.x : limits.maxX - this.radius*2 - (this.x - limits.minX);
+    } else {
+        //vertical
+        currX = options.mirror ? limits.maxX - this.radius*2 - (this.x - limits.minX): this.x;
+        currY = this.y;
+    }
+    
+    options = options || {};
+    options.radius = this.radius;
+    options.borderColor = this.borderColor;
+    options.borderWidth = this.borderWidth;
+    options.fillColor = (options.kls && options.kls === 'blegenda'? 'none' : DIATONIC.map.color.fill );
+    options.openColor = (options.kls && options.kls === 'blegenda'? DIATONIC.map.color.open : 'none' );
+    options.closeColor = (options.kls && options.kls === 'blegenda'? DIATONIC.map.color.close : 'none' );
+    
+    this.SVG.gid = printer.printButton( id, currX, currY, options );
+
+};
+
+DIATONIC.map.Button.prototype.clear = function(delay) {
+    if(!this.SVG.button ) return;
+    var that = this;
+    if(delay) {
+        window.setTimeout(function(){ that.clear(); }, delay*1000);
+        return;
+    }    
+    this.SVG.closeArc.style.setProperty( 'fill', 'none' );
+    this.SVG.openArc.style.setProperty( 'fill', 'none' );
+};
+
+DIATONIC.map.Button.prototype.setOpen = function(delay) {
+    if(!this.SVG.button ) return;
+    var that = this;
+    if(  delay ) {
+        window.setTimeout(function(){that.setOpen();}, delay*1000 );
+        return;
+    } 
+    this.SVG.openArc.style.setProperty( 'fill', DIATONIC.map.color.open );
+};
+
+DIATONIC.map.Button.prototype.setClose = function(delay) {
+    if(!this.SVG.button ) return;
+    var that = this;
+    if(  delay ) {
+        window.setTimeout(function(){that.setClose();}, delay*1000);
+        return;
+    } 
+    this.SVG.closeArc.style.setProperty( 'fill', DIATONIC.map.color.close );
+};
+
+DIATONIC.map.Button.prototype.setSVG = function(showLabel, pull, push, translator ) {
+    var b = this.SVG;
+    this.SVG.button = document.getElementById(b.gid);
+    this.SVG.openArc = document.getElementById(b.gid+'_ao');
+    this.SVG.openText = document.getElementById(b.gid+'_to');
+    this.SVG.closeArc = document.getElementById(b.gid+'_ac');
+    this.SVG.closeText = document.getElementById(b.gid+'_tc');
+    
+    if( translator ) {
+        this.SVG.openText.setAttribute( 'data-translate', pull );
+        this.SVG.closeText.setAttribute( 'data-translate', push );
+        this.setText(showLabel, translator.getResource(pull), translator.getResource(push) ); 
+} else {
+        this.setText(showLabel, pull, push ); 
+    }
+    
+};
+
+DIATONIC.map.Button.prototype.setText = function( showLabel, open, close ) {
+    if(this.SVG.openText) {
+        this.SVG.openText.textContent = open ? open : this.getLabel( this.openNote, showLabel );
+        this.SVG.closeText.textContent = close ? close : this.getLabel( this.closeNote, showLabel );
+    }    
+};
+
+DIATONIC.map.Button.prototype.getLabel = function(nota, showLabel) {
+    var l = nota.key;
+    
+    if (showLabel) {
+        l = l.toUpperCase() + '';
+        l = ABCXJS.parse.key2br[l].toUpperCase();
+    }
+    
+    if ( nota.isChord ) {
+       l = l.toLowerCase() + '';
+    }    
+    
+    if( nota.isMinor ) {
+        l+='-';
+    }
+    return l;
+};
+
