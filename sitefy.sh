49a86e526de0a402c993b2b420508c5da9b2aab0
Flavio Vani <flavio.vani@gmail.com>
2020-06-28 18:13:05 -0300

    Aumentando repertorio club

--- sitefy.sh
+++ sitefy.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
-tar czvf site.tar.gz ./site ./soundfont ./ace4abcx ./fontsGoogle ./fontsIco ./chords ./accordions ./tabs ./images \
-                     ./css ./html ./src ./languages ./diatonic ./file ./practices ./songs ./abcxjs ./jslib ./jquery \
-                     ./.htaccess ./mapa.html ./index.html ./mapa-5.00.html ./mapa-debug.html
+#tar czvf site.tar.gz ./site ./soundfont ./ace4abcx ./fontsGoogle ./fontsIco ./chords ./accordions ./tabs ./images \
+#                     ./css ./html ./src ./languages ./diatonic ./file ./practices ./songs ./abcxjs ./jslib ./jquery \
+#                     ./.htaccess ./mapa.html ./index.html ./mapa-5.00.html ./mapa-debug.html
+
+
+
+FILES="./site ./soundfont ./ace4abcx ./fontsGoogle ./fontsIco ./chords ./accordions ./tabs ./images \
+       ./css ./html ./src ./languages ./diatonic ./file ./practices ./songs ./abcxjs ./jslib ./jquery \
+       ./.htaccess ./mapa.html ./index.html ./mapa-5.00.html ./mapa-debug.html"
+
+for f in $FILES
+do
+	echo "Processing $f"
+    cp -pr $f ../site-hostinger/
+done
