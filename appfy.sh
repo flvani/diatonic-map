79c205961a4b17053314d880d52f63b54b39f56f
Flávio Vani <flavio.vani@gmail.com>
2024-01-21 19:34:01 -0300

    update fix appfy and sitefy shells

--- appfy.sh
+++ appfy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-v_abcx=6.24
+v_abcx=6.21
 v_app=2.1.24
 assets_folder="/home/flavio/Projetos/diatonic-app/app/src/main/assets"
 
@@ -11,9 +11,16 @@ FILES="\
 ./jquery/jquery-3.6.0.min.js ./jslib/MIDI_5.25-min.js \
 ./jslib/proto_noconflict.js ./jslib/html5kellycolorpicker.min.js ./jslib/waterbug.js"
 
-FOLDERS="./accordions ./chords ./songs ./practices ./fontsGoogle ./fontsIco ./images ./languages ./privacidade ./privacy ./soundfont ./html"
+FOLDERS="./accordions ./chords ./practices ./fontsGoogle ./fontsIco ./images ./languages ./privacidade ./privacy ./soundfont ./html"
 
 rm -rf $assets_folder/*
+mkdir -p $assets_folder/songs
+#mkdir -p $assets_folder/abcxjs
+#mkdir -p $assets_folder/diatonic
+#mkdir -p $assets_folder/file
+#mkdir -p $assets_folder/css
+
+cp -pr songs/*repertorio.abcx $assets_folder/songs/
 
 for f in $FILES
 do
--- sitefy.sh
+++ sitefy.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
+assets_folder="../site-hostinger"
 
 FILES="./privacy ./privacidade ./app ./site ./soundfont ./ace4abcx ./fontsGoogle ./fontsIco ./chords ./accordions ./tabs ./images \
        ./css ./html ./src ./languages ./diatonic ./file ./practices ./repertorio ./abcxjs ./jslib ./jquery \
        ./.htaccess ./index.html ./mapa-debug.html ./app.html ./app-debug.html"
 
+rm -rf $assets_folder/*
+mkdir -p $assets_folder/songs 
+cp -pr songs/*repertorio.abcx $assets_folder/songs/
+
 for f in $FILES
 do
 	echo "Processing $f"
-    rm -r ../site-hostinger/$f
-    cp -pr $f ../site-hostinger/
+    cp -pr $f $assets_folder/
 done
-
-mkdir -p ../site-hostinger/songs 
-rm ../site-hostinger/songs/*
-cp -pr songs/*repertorio.abcx ../site-hostinger/songs/
