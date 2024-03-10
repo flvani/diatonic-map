95eebd1917d76447028eb340d3c2aca5875d4e30
Flávio Vani <flavio.vani@gmail.com>
2024-03-09 22:03:27 -0300

    absolute urls to privacy texts

--- appfy.sh
+++ appfy.sh
@@ -9,7 +9,7 @@ FILES="\
     ./abcxjs/abcxjs_${v_abcx}-min.js ./diatonic/diatonic_${v_abcx}-min.js \
     ./file/filemanager_${v_abcx}-min.js ./css/modal.css ./css/styles4abcx_${v_abcx}-min.css \
     ./jslib/jquery-3.7.1.min.js ./jslib/MIDI_5.25-min.js \
-    .jslib/enjoyhint.min.js jslib/jquery-scrollto-2.1.3.min.js jslib/kinetic-v5.1.0.js css/enjoyhint.css\
+    ./jslib/enjoyhint.min.js jslib/jquery-scrollto-2.1.3.min.js jslib/kinetic-v5.1.0.js css/enjoyhint.css\
     ./jslib/proto_noconflict.js ./jslib/html5kellycolorpicker.min.js ./jslib/waterbug.js"
 
 FOLDERS="\
